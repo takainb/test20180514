455e0a6db4a076b18503036a904f4305ce7cd119
takai <takai@192.168.1.14>
2018-05-15 11:30:11 +0900

    init

--- rtc/WebContent/main.js
+++ rtc/WebContent/main.js
@@ -1,48 +1,79 @@
-// videoタグを取得
-var video = document.getElementById("video")
-// 取得するメディア情報を指定
-var medias = { audio:false, video:{}};
-if(!!navigator.mediaDevices&&!!navigator.mediaDevices.getUserMedia) {
-	// 対応環境
-	// リアカメラを指定
-	//medias.video.facingMode = {exact:"enviroment"};
-	// getUserMediaを用いて、webカメラの映像を取得
-	navigator.mediaDevices.getUserMedia(medias).then(
-			function(stream) {
-				// videoタグのソースにwebカメラの映像を指定
-				video.srcObject = stream;
-			}
-	).catch(
-			function(err){
-				// カメラの許可がされなかった場合にエラー
-				window.alert("カメラの使用が許可されませんでした");
-			}
-	);
-	
-} else {
-    // 非対応環境
-	window.alert("非対応環境です");
+const video = document.getElementById("video");
+const dispCs = document.getElementById("disp");
+dispCs.width = 128
+dispCs.height = 128;
+const dispCtx = dispCs.getContext('2d');
+
+const hiddenCanvas = document.createElement('canvas');
+hiddenCanvas.width = 128;
+hiddenCanvas.height = 128;
+const hiddenCtx = hiddenCanvas.getContext('2d');
+
+function start() {
+    if (navigator.mediaDevices.getUserMedia) {
+        //alert("navigator.mediaDevices.getUserMedia");
+        let settings;
+        if(document.getElementsByName("camtype")[0].checked === true) {
+            settings = {video: {
+                    facingMode: {
+                        exact: "environment"
+                    }
+                }};
+        } else {
+            settings = {video: true};
+        }
+        navigator.mediaDevices.getUserMedia(settings)
+            .then(
+                function(stream) {
+                    video.srcObject = stream;
+                    video.play();
+                    streaming = true;
+                    
+                    requestAnimationFrame(myDraw);
+                })
+            .catch(function(err) {
+                alert("An error occured! " + err);
+            });
+    }
 }
 
-var canvas = document.getElementById("canvas");
-video.addEventListener("loadedmetadata",function(e) {
-	
-	var ctx = canvas.getContext("2d");
-	canvas.width = video.videoWidth;
-	canvas.height = video.videoHeight;
-	
-	// Context（カメラ左右）を反転　※フロントカメラの場合
-	//ctx.translate(canvas.width,0);
-	//ctx.scale(-1,1);
-	
-	setInterval(function(e) {
-		ctx.drawImage(video,0,0,canvas.width,canvas.height);
-		// ピクセルへのアクセス
-		var imagedata = ctx.getImageData(0,0,canvas.width,canvas.height);
-	},33);	
-});
+function stop() {
+    console.log("stop start");
+    if (video.srcObject) {
+        video.pause();
+        video.srcObject = null;
+    } else {
+        if (localstream) {
+            localstream.stop();
+        }
+    }
+    console.log("stop end");
+}
 
+function myDraw() {
+    hiddenCtx.drawImage(video, 0, 0, dispCs.width, dispCs.height);
+    dispCtx.drawImage(video, 0, 0, dispCs.width, dispCs.height);
 
+    //var s = (new Date()).getTime();
+    //showMsg("Detecting ...");
 
+    // 顔検出
+    var comp = ccv.detect_objects({
+        //"canvas": ccv.grayscale(ccv.pre(video)),
+        "canvas": ccv.grayscale(hiddenCanvas),
+        "cascade": cascade,
+        "interval": 5,
+        "min_neighbors": 1
+    });
 
-
+    //showMsg("Elapsed time : " + ((new Date()).getTime() - s).toString() + "ms");
+    dispCtx.lineWidth = 3;
+    dispCtx.strokeStyle = "#f00";
+    for (var i = 0; i < comp.length; i++) {
+        dispCtx.strokeRect(comp[i].x, comp[i].y, comp[i].width, comp[i].height);
+        //ctx2.drawImage(img,comp[i].x, comp[i].y, comp[i].width, comp[i].height,0,0,160,120);
+    }
+    requestAnimationFrame(myDraw);
+    //setTimeout(myDraw, 500);
+    //setImmediate(myDraw);
+}
